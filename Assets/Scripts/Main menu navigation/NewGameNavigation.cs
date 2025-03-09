15b2cf22c16053e3cfaa7bbfa50aad2176873a21
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-03-09 19:09:08 +0800

    feat: made new game async

--- Assets/Scripts/Main menu navigation/NewGameNavigation.cs
+++ Assets/Scripts/Main menu navigation/NewGameNavigation.cs
@@ -5,7 +5,7 @@ public class NewGameNavigation : MonoBehaviour
     public void NewGame()
     {
 
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainScene");
         UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("MainMenu");
 
     }
