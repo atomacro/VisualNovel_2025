bc0180d5c088823760e7f1912c12c2a30830f312
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-03-09 20:28:25 +0800

    Asset: update fonts, improved new game

--- Assets/Scripts/Main menu navigation/NewGameNavigation.cs
+++ Assets/Scripts/Main menu navigation/NewGameNavigation.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class NewGameNavigation : MonoBehaviour
 {
     public void NewGame()
     {
+        StartCoroutine(LoadMainSceneAsync());
+    }
 
-        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainScene");
+    private IEnumerator LoadMainSceneAsync()
+    {
+        AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainScene");
         UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("MainMenu");
 
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log(asyncLoad.progress);
+            yield return null;
+        }
     }
 }
