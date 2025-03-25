e050b4b5bd1a6543ef81ee128d5b402b20bc7e53
vivrelavie <stefaniegabion07@gmail.com>
2025-03-25 17:59:53 +0800

    feat: Save and Load functionalities

---
+++ Assets/Scripts/Main menu functionalities/CharacterData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterData
+{
+        public string characterName;
+        public string spriteName;
+        public Vector2 position;
+}
